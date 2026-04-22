64903dd55a5b85011d4ad3a02732f0cda6603350
X-Bows Tech <41098278+XBowsTech@users.noreply.github.com>
2026-04-22 15:19:46 +0800

    Update keyboards/xbows/numpad_v2/keymaps/default/keymap.c
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/xbows/numpad_v2/keymaps/default/keymap.c
+++ keyboards/xbows/numpad_v2/keymaps/default/keymap.c
@@ -16,18 +16,18 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT(
-    KC_ESC ,  KC_TAB,   KC_BSPC,  MO(1),
-    KC_NUM,   KC_PSLS,  KC_PAST,  KC_PMNS,
-    KC_P7,    KC_P8,    KC_P9,    KC_PPLS,
-    KC_P4,    KC_P5,    KC_P6,    
-    KC_P1,    KC_P2,    KC_P3,    KC_PENT,
-         KC_P0,       KC_PDOT),
-  [1] = LAYOUT(
-    QK_BOOT,   EE_CLR,    KC_TRNS,   KC_TRNS,
-    KC_CALC,   KC_MYCM,   KC_MSEL,   KC_MAIL,
-    RM_NEXT,   RM_VALU,   RM_HUEU,   KC_TRNS,
-    RM_SPDD,   RM_TOGG,   RM_SPDU,   
-    KC_TRNS,   RM_VALD,   KC_TRNS,   KC_TRNS,
-        KC_TRNS,          KC_TRNS)
+  [0] = LAYOUT_numpad_6x4(
+    KC_ESC,  KC_TAB,  KC_BSPC, MO(1),
+    KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,
+    KC_P7,   KC_P8,   KC_P9,
+    KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
+    KC_P1,   KC_P2,   KC_P3,
+        KC_P0,        KC_PDOT, KC_PENT),
+  [1] = LAYOUT_numpad_6x4(
+    QK_BOOT, EE_CLR,  KC_TRNS, KC_TRNS,
+    KC_CALC, KC_MYCM, KC_MSEL, KC_MAIL,
+    RM_NEXT, RM_VALU, RM_HUEU,
+    RM_SPDD, RM_TOGG, RM_SPDU, KC_TRNS,
+    KC_TRNS, RM_VALD, KC_TRNS,
+        KC_TRNS,      KC_TRNS, KC_TRNS)
 };
